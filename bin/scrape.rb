b7f10d8a50c0609abfcdf11f77ffefc1fb2e7e9a
dave@kapoq.com <dave@kapoq.com>
2011-01-23 22:56:22 +0100

    Fixed up srape

--- bin/scrape.rb
+++ bin/scrape.rb
@@ -1,20 +1,14 @@
 #!/usr/bin/env ruby
-
 require File.dirname(__FILE__) + '/../lib/mulberry.rb'
 
-# TODO: extract:
-# * distance of time in minutes of next service
-# * any special notifications (train)
-# * name of vehicle
-# Create partials and layout
-
-SOURCES = YAML.load(open File.join(DATA_DIR, '/sources.yml')).map { |s| Source.new s }
+SOURCES = YAML.load(open File.join(DATA_DIR, '/sources.yml'))
 
 html = ""
 SOURCES.each do |s|
-  s.scrape
-  html << "<h1>#{s.location} to #{s.direction} via #{s.vehicle}</h1>"
-  html << s.content.map(&:inner_html).join("<hr>")
+  puts "Scraping #{s.name}"
+  html << "<h1>#{s.location} to #{s.direction} via #{s.vehicle} #{s.service}</h1>"
+  html << s.content.inner_html
+  html << "<hr>"
 end
 
 File.open(File.join(ROOT, 'public', "timetable_#{Time.now}.html"), "w") { |f| f << html }
