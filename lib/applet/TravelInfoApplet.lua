5e07178c0991d5a1d6d5015e8b189d7f08a9a6b1
dave@kapoq.com <dave@kapoq.com>
2011-05-10 11:39:53 +0200

    Added hacky SqueezeOS applet to render entries

---
+++ lib/applet/TravelInfoApplet.lua
@@ -0,0 +1,96 @@
+--[[
+=head1 NAME
+
+applets.TravelInfo.TravelApplet - Live Travel Info Applet
+
+=head1 DESCRIPTION
+
+Displays live departure board
+
+=head1 FUNCTIONS
+
+Applet related methods are described in L<jive.Applet>. 
+
+=cut
+--]]
+
+
+-- stuff we use
+local oo                     = require("loop.simple")
+local string                 = require("string")
+local io                     = require("io")
+
+local Applet                 = require("jive.Applet")
+local Window                 = require("jive.ui.Window")
+local Textarea               = require('jive.ui.Textarea')
+local Label                  = require('jive.ui.Label')
+local SimpleMenu             = require("jive.ui.SimpleMenu")
+local Icon                   = require("jive.ui.Icon")
+local System                 = require("jive.System")
+local Surface                = require("jive.ui.Surface")
+
+local tostring               = tostring
+local require                = require
+local package                = package
+local pairs                  = pairs
+local table                  = table
+
+local entries = {}
+local entries_filename = "applets/TravelInfo/entries"
+
+function Entry(e) entries[e.source] = e end
+
+module(...)
+oo.class(_M, Applet)
+
+function menu(self, menuItem)
+   local menu  = SimpleMenu("menu")
+
+   require(entries_filename)
+   package.loaded[entries_filename] = nil
+
+   for _, entry in pairs(entries) do
+      local image = Surface:loadImage("applets/TravelInfo/" .. entry.vehicle .. ".png")
+      local icon  = Icon("icon", image)
+      menu:addItem({
+                      text = entry.title,
+                      weight = entry.weight,
+                      icon = icon,
+                      sound = "WINDOWSHOW",
+                      callback = function()
+                                    self:showEntry(entry)
+                                 end
+                   })
+   end
+   
+   local window = Window("window", "Travel")
+
+   menu:setComparator(menu.itemComparatorWeightAlpha)
+   window:addWidget(menu)
+   window:show()
+end
+
+function showEntry(self, entry)
+   -- TODO: timer and reload
+
+   local menu = SimpleMenu("menu", {
+                              { text = "Back",
+                                sound = "WINDOWHIDE",
+                                callback = function()
+                                              window:hide()
+                                           end
+                             }
+                           })
+
+
+   require(entries_filename)
+   package.loaded[entries_filename] = nil
+
+   local e        = entries[entry.source]
+   local window   = Window("text_list", e.title) 
+   local textarea = Textarea("text", (e.description .. "\n\n----\n" .. e.departures .. "\n\n----\nUpdated at: " .. e.updated_at))
+
+   window:addWidget(textarea)
+   window:addWidget(menu)
+   window:show()
+end
---
+++ lib/applet/TravelInfoMeta.lua
@@ -0,0 +1,24 @@
+local oo            = require("loop.simple")
+local AppletMeta    = require("jive.AppletMeta")
+local appletManager = appletManager
+local jiveMain      = jiveMain
+
+module(...)
+oo.class(_M, AppletMeta)
+
+function jiveVersion(meta)
+   return 1, 1
+end
+
+function defaultSettings(meta)
+   return {
+      currentSetting = 0,
+   }
+end
+
+function registerApplet(meta)
+   jiveMain:addItem(meta:menuItem('travelInfoApplet', 'home', "TRAVELINFO", function(applet, ...) applet:menu(...) end, 20))
+end
+
+
+
